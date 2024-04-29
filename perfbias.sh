813eb0fd4b1c733603f2c0ff5689c6ee089295aa
Artem S. Tashkinov <birdie-github@users.noreply.github.com>
2024-04-29 11:16:22 +0000

    Extend, simplify, improve

--- perfbias.sh
+++ perfbias.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# 2024-03-20 20:12:41 - first release
+# 2024-04-28 23:21:29 - simplify and improve, rewrite in pure bash
+
+# "default" is specified but doesn't work, wtf?
+modes='performance|balance_performance|balance_power|power'
+
 cpus=`ls /sys/devices/system/cpu/cpufreq/*/energy_performance_preference`
 test -z "$cpus" && echo "WTF?" && exit 1
 
@@ -18,18 +24,16 @@ setter()
 now=`getter`
 echo "Now: $now"
 
-if [ "$1" = "performance" -a "$now" != "performance" ]; then
-    setter performance
-elif [ "$1" = "power" -a "$now" != "power" ]; then
-    setter power
+if [[ "$1" =~ $modes && "$now" != "$1" ]]; then
+    setter "$1"
 elif [ "$1" = "switch" ]; then
-    if [ "$now" = "power" ]; then
+    if [[ "$now" =~ "power" ]]; then
         setter performance
     else
         setter power
     fi
 elif [ -z "$1" ]; then
-    echo "Use is: `basename $0` [performance|power|switch]"
+    echo "Use is: `basename $0` [$modes|switch]"
 else
     echo "There's nothing to do"
 fi
